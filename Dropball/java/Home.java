3344168965fd183026cad12b45964f28ef081a94
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2024-03-04 03:36:32 +0800

    Create Home.java

---
+++ Dropball/java/Home.java
@@ -0,0 +1,65 @@
+package com.example.dropball;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void goToDevelopers(View v){
+        Intent i = new Intent(this, Developers.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goToInstructions(View v){
+        Intent i = new Intent(this, Instructions.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void goToGame(View v){
+        Intent i = new Intent(this, Game.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showExitConfirmationDialog();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void showExitConfirmationDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.exit_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+        Button btnOkay = dialogView.findViewById(R.id.btnOkay);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        btnCancel.setOnClickListener(v -> alertDialog.dismiss());
+        btnOkay.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+}
