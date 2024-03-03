505beaeabbb952f225fd8c4ae9b15e0e6dec8f19
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2024-03-04 03:38:57 +0800

    Create Instructions2.java

---
+++ Dropball/java/Instructions2.java
@@ -0,0 +1,53 @@
+package com.example.dropball;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+
+public class Instructions2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructions2);
+    }
+
+    public void goToInstructions3(View v){
+        Intent i = new Intent(this, Instructions3.class);
+        startActivity(i);
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            showExitConfirmationDialog();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+    private void showExitConfirmationDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = getLayoutInflater().inflate(R.layout.exit_dialog, null);
+        dialogBuilder.setView(dialogView);
+
+        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
+        Button btnOkay = dialogView.findViewById(R.id.btnOkay);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        btnCancel.setOnClickListener(v -> alertDialog.dismiss());
+        btnOkay.setOnClickListener(v -> {
+            finish();
+        });
+    }
+
+}
