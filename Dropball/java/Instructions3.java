5bd080463c1a4ab645bde323def0320a649cd1bd
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2024-03-05 06:29:00 +0800

    Update Instructions3.java

--- Dropball/java/Instructions3.java
+++ Dropball/java/Instructions3.java
@@ -1,13 +1,11 @@
 package com.example.dropball;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
 
 public class Instructions3 extends AppCompatActivity {
 
@@ -29,31 +27,15 @@ public class Instructions3 extends AppCompatActivity {
         finish();
     }
 
+
+
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            showExitConfirmationDialog();
+            // Navigate back to MainActivity without showing the exit confirmation dialog
+            goToInstructions2(null);
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
-    private void showExitConfirmationDialog() {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        View dialogView = getLayoutInflater().inflate(R.layout.exit_dialog, null);
-        dialogBuilder.setView(dialogView);
-
-        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
-        Button btnOkay = dialogView.findViewById(R.id.btnOkay);
-
-        AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-
-        btnCancel.setOnClickListener(v -> alertDialog.dismiss());
-        btnOkay.setOnClickListener(v -> {
-            finish();
-        });
-    }
-
 }
