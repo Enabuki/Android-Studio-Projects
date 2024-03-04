87b2a0ada44de5b2aada110b1e3c546d6658e47a
Andrei Jefferson S. Aseron <111546580+Enabuki@users.noreply.github.com>
2024-03-05 06:27:07 +0800

    Update Home.java

--- Dropball/java/Home.java
+++ Dropball/java/Home.java
@@ -1,13 +1,13 @@
 package com.example.dropball;
 
-import androidx.appcompat.app.AlertDialog;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.Button;
+
 
 public class Home extends AppCompatActivity {
 
@@ -34,32 +34,19 @@ public class Home extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+    public void goToMain(View v){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slide_in_bottom, R.anim.slide_out_bottom);
+        finish();
+    }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK) {
-            showExitConfirmationDialog();
+            goToMain(null);
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-
-
-    private void showExitConfirmationDialog() {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        View dialogView = getLayoutInflater().inflate(R.layout.exit_dialog, null);
-        dialogBuilder.setView(dialogView);
-
-        Button btnCancel = dialogView.findViewById(R.id.btnCancel);
-        Button btnOkay = dialogView.findViewById(R.id.btnOkay);
-
-        AlertDialog alertDialog = dialogBuilder.create();
-        alertDialog.show();
-
-        btnCancel.setOnClickListener(v -> alertDialog.dismiss());
-        btnOkay.setOnClickListener(v -> {
-            finish();
-        });
-    }
-
 }
